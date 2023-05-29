98f0704631260382ec9074fcc078a64efa5f9abf
Andrii Khoma <anik123wb@gmail.com>
2023-05-29 15:41:16 +0300

    products list

--- app/[locale]/products/productsList.tsx
+++ app/[locale]/products/productsList.tsx
@@ -2,9 +2,12 @@
 
 import {useEffect, useState} from "react";
 import {useTranslations, useFormatter, useLocale} from 'next-intl';
+import Link from "next-intl/link";
 import {fetchProducts, ProductsListType} from "@/utils/fetchProducts";
 import {AttributeDto} from "@/shop-shared/dto/product/attribute.dto";
 import {CategoryDto} from "@/shop-shared/dto/category/category.dto";
+import Image from "next/image";
+import {ProductDto} from "@/shop-shared/dto/product/product.dto";
 
 export default function ProductsList({defaultList, attributes, categories}: {
     defaultList: ProductsListType,
@@ -28,64 +31,70 @@ export default function ProductsList({defaultList, attributes, categories}: {
         void updateProducts();
     }, []);
 
-    return <div>
+    const drawColorAttributes = (product: ProductDto) => {
+        const attr = product.attrs['color'];
+        if (!attr) {
+            return null;
+        }
+
+        return <div className="flex gap-2">
+            {attr.map(value => (
+                <Link href={`/product/${product.id}?color=${value}`} key={value}>
+                    <div className="
+                        w-6 h-6 border-2
+                        border-gray-300 hover:border-gray-600
+                        dark:border-gray-700 hover:dark:border-gray-400
+                    " style={{backgroundColor: value}}></div>
+                </Link>
+            ))}
+        </div>
+    }
+
+    const drawSizeAttributes = (product: ProductDto) => {
+        const attr = product.attrs['size'];
+        if (!attr) {
+            return null;
+        }
+        const attribute = attributes.find(a => a.key === 'size');
+        if (!attribute) {
+            return null;
+        }
+
+        return <div className="flex gap-2 ml-auto">
+            {attr.map(value => (
+                <div key={value}>{attribute.values.find(val => val.key === value)?.title}</div>
+            ))}
+        </div>
+    }
+
+    return <div className="flex gap-2">
         Products list {t('hello')}
         {products.length}
         {products.map(product => (
-            <div key={product.id} className="bg-amber-50">
-                <div className="flex font-sans">
-                    <div className="flex-none w-48 relative">
-                        <img src="https://picsum.photos/200/300" alt=""
-                             className="absolute inset-0 w-full h-full object-cover" loading="lazy"/>
+            <div key={product.id} className="border-2 border-gray-300 dark:border-gray-700">
+                <Link href={`/product/${product.id}`}>
+                    <Image
+                        src="https://picsum.photos/200/200"
+                        alt={product.title}
+                        width={300}
+                        height={300}
+                        loading="lazy"
+                    />
+                </Link>
+                <div className="p-3">
+                    <div className="flex">
+                        {drawColorAttributes(product)}
+                        {drawSizeAttributes(product)}
                     </div>
-                    <form className="flex-auto p-6">
-                        <div className="flex flex-wrap">
-                            <h1 className="flex-auto text-lg font-semibold text-slate-900">
-                                {product.title}
-                            </h1>
-                            <div className="text-lg font-semibold text-slate-500">
-                                ${product.price}
-                            </div>
-                            <div className="w-full flex-none text-sm font-medium text-slate-700 mt-2">
-                                In stock
-                            </div>
+                    <div className="flex flex-wrap">
+                        <h1 className="flex-auto text-sm font-medium text-slate-700 dark:text-slate-200 mt-1">
+                            {product.title}
+                        </h1>
+                        <div className="text-lg font-semibold text-slate-800 dark:text-slate-100">
+                            ${product.price}
                         </div>
-                        <div className="flex space-x-4 mb-6 text-sm font-medium">
-                            <div className="flex-auto flex space-x-4">
-                                <button className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-                                        type="submit">
-                                    Buy now
-                                </button>
-                                <button
-                                    className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900"
-                                    type="button">
-                                    Add to bag
-                                </button>
-                            </div>
-                            <button
-                                className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200"
-                                type="button" aria-label="Like">
-                                <svg width="20" height="20" fill="currentColor" aria-hidden="true">
-                                    <path fillRule="evenodd" clipRule="evenodd"
-                                          d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"/>
-                                </svg>
-                            </button>
-                        </div>
-                        <p className="text-sm text-slate-700">
-                            Free shipping on all continental US orders.
-                        </p>
-                    </form>
+                    </div>
                 </div>
-
-                {/*<p>{product.title}</p>*/}
-                {/*/!*<Image*!/*/}
-                {/*/!*    src={product.image}*!/*/}
-                {/*/!*    alt={product.title}*!/*/}
-                {/*/!*    width={128}*!/*/}
-                {/*/!*    height={128}*!/*/}
-                {/*/!*></Image>*!/*/}
-                {/*<div>${product.price}</div>*/}
-                {/*<div>{product.description}</div>*/}
             </div>
         ))}
     </div>
--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const withNextIntl = require('next-intl/plugin')(
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 }
 
 module.exports = withNextIntl(nextConfig)
